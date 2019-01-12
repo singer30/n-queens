dae03ad1b04ad7207b05db9c6e172ea0fb751e59
singer30 <lukas.w.singer@gmail.com>
2019-01-11 16:58:18 -0800

    Implement working code for Board.js helper functions

--- src/Board.js
+++ src/Board.js
@@ -79,57 +79,206 @@
     //
     // test if a specific row on this board contains a conflict
     hasRowConflictAt: function(rowIndex) {
-      return false; // fixme
+      //create sum variable and assign to zero
+      var sum = 0;
+      //get row at rowIndex
+      var row = this.rows()[rowIndex];
+      //loop through row
+      for (var i = 0; i < row.length; i++) {
+        //add each item to sum
+        sum += row[i];
+      }
+      //if sum is greater than 1
+      if(sum > 1) {
+        return true
+      } else { 
+        return false
+      }
     },
-
     // test if any rows on this board contain conflicts
     hasAnyRowConflicts: function() {
-      return false; // fixme
+      // create a variable and assign it to the rows.
+      var rows = this.rows(); 
+      // create a variable result and set it equal to false;
+      var any = false;
+      // loop through the variabls of rows. (i.e. all the arrays);
+      for (var i = 0; i < rows.length; i++) {
+        // if the result of hasRowConflictAt returns true;
+        if (this.hasRowConflictAt(i)) {
+          //change the result variable to true and return it. 
+          any = true;
+        }
+      }
+      // return result;
+      return any;
     },
-
-
 
     // COLUMNS - run from top to bottom
     // --------------------------------------------------------------
     //
     // test if a specific column on this board contains a conflict
     hasColConflictAt: function(colIndex) {
-      return false; // fixme
+      //create sum variable and assign to zero
+      var sum = 0;
+      //create rows and assign to this.rows()
+      var rows = this.rows();
+      //loop through rows
+      for (var i = 0; i < rows.length; i++) {
+        //add each item at colIndex of row to sum
+        sum += rows[i][colIndex];
+      }
+      //if sum is greater than 1
+      if(sum > 1) {
+        return true
+      } else { 
+        return false
+      }
     },
 
     // test if any columns on this board contain conflicts
     hasAnyColConflicts: function() {
-      return false; // fixme
+      // create a variable and assign it to the rows.
+      var rows = this.rows(); 
+      // create a variable result and set it equal to false;
+      var any = false;
+      // loop through the variabls of rows. (i.e. all the arrays);
+      for (var i = 0; i < rows.length; i++) {
+        // if the result of hasRowConflictAt returns true;
+        if (this.hasColConflictAt(i)) {
+          //change the result variable to true and return it. 
+          any = true;
+        }
+      }
+      // return result;
+      return any;
     },
-
-
 
     // Major Diagonals - go from top-left to bottom-right
     // --------------------------------------------------------------
     //
     // test if a specific major diagonal on this board contains a conflict
     hasMajorDiagonalConflictAt: function(majorDiagonalColumnIndexAtFirstRow) {
-      return false; // fixme
+      //create mDx and assign to majorDiagonalColumnIndexAtFirstRow
+      var mDx = majorDiagonalColumnIndexAtFirstRow;
+      //create rows and assign to this.rows()
+      var rows = this.rows();
+      //create sum and assign to zero
+      var sum = 0;
+      
+      //if mDx equals zero
+      if (mDx === 0) {
+        //loop through rows
+        for (var i = 0; i < rows.length; i++) {
+          //add to sum the item at index i + 1 of row i rows[i][mDx + i]
+          sum += rows[i][i];
+        }
+      }
+      
+      //if mDx is less than 0
+      if (mDx < 0) { 
+        //loop through rows setting i equal to -mDx
+        for (var i = -mDx; i < rows.length; i++) {
+          //add to sum the item at index i - 1 of row i 
+          sum += rows[i][i + mDx];
+        }
+      } 
+      
+      //if mDx is greater than 0
+      if (mDx > 0) {
+        //loop through rows setting i = 0;
+        for (var i = 0; i < rows.length - mDx; i++) {
+          //add to sum the item at index mDx + i of row i
+          sum += rows[i][mDx + i];
+        }
+      }
+        
+      if (sum > 1) {
+        return true;
+      } else {
+        return false;
+      }
     },
 
     // test if any major diagonals on this board contain conflicts
     hasAnyMajorDiagonalConflicts: function() {
-      return false; // fixme
+      // create a variable and assign it to the rows.
+      var rows = this.rows(); 
+      // create a variable result and set it equal to false;
+      var any = false;
+      // loop through the rows setting i equal to -(length - 1)
+      for (var i = 1 - rows.length; i < rows.length; i++) {
+        // if the result of hasMajorDiagonalConflictAt returns true;
+        if (this.hasMajorDiagonalConflictAt(i)) {
+          //change the result variable to true and return it. 
+          any = true;
+        }
+      }
+      // return result;
+      return any;
     },
-
-
 
     // Minor Diagonals - go from top-right to bottom-left
     // --------------------------------------------------------------
     //
     // test if a specific minor diagonal on this board contains a conflict
     hasMinorDiagonalConflictAt: function(minorDiagonalColumnIndexAtFirstRow) {
-      return false; // fixme
+      //create mDx and assign to majorDiagonalColumnIndexAtFirstRow
+      var mDx = minorDiagonalColumnIndexAtFirstRow;
+      //create rows and assign to this.rows()
+      var rows = this.rows();
+      //create sum and assign to zero
+      var sum = 0;
+      
+      //if mDx equals length minus 1
+      if (mDx === rows.length - 1) {
+        //loop through rows
+        for (var i = 0; i < rows.length; i++) {
+          //add to sum the item at index mDx - i of row i
+          sum += rows[i][mDx - i];
+        }
+      }
+      
+      //if mDx is less than length minus 1
+      if (mDx < rows.length - 1) { 
+        //loop through rows setting i equal to -mDx
+        for (var i = 0; i < mDx + 1; i++) {
+          //add to sum the item at index i - 1 of row i 
+          sum += rows[i][mDx - i];
+        }
+      } 
+      
+      //if mDx is greater than length minus 1
+      if (mDx > rows.length - 1) {
+        //loop through rows setting i = 0;
+        for (var i = mDx - rows.length + 1; i < rows.length; i++) {
+          //add to sum the item at index mDx + i of row i
+          sum += rows[i][mDx - i];
+        }
+      }
+        
+      if (sum > 1) {
+        return true;
+      } else {
+        return false;
+      }
     },
 
     // test if any minor diagonals on this board contain conflicts
     hasAnyMinorDiagonalConflicts: function() {
-      return false; // fixme
+      // create a variable and assign it to the rows.
+      var rows = this.rows(); 
+      // create a variable result and set it equal to false;
+      var any = false;
+      // loop through the rows setting i equal to 0 up to length*2 -1
+      for (var i = 0; i < (rows.length * 2) - 1; i++) {
+        // if the result of hasMajorDiagonalConflictAt returns true;
+        if (this.hasMinorDiagonalConflictAt(i)) {
+          //change the result variable to true and return it. 
+          any = true;
+        }
+      }
+      // return result;
+      return any;
     }
 
     /*--------------------  End of Helper Functions  ---------------------*/
